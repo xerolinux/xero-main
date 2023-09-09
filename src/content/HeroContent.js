a9451f1cec61f10a9f9a4d30d8b20ccf01493c4c
godalming123 <jamiesamglasson@gmail.com>
2023-09-09 16:55:36 +0100

    The smallest grammer fix known to man...

--- src/content/HeroContent.js
+++ src/content/HeroContent.js
@@ -1,7 +1,7 @@
 export const CONTENT = {
   heading: "XeroLinux",
   text: `A community maintained Arch-Based Distro,
-              that provides you with an easy way to install Arch and a well optimized, beautifully Customized Plasma Desktop, that you can later shape to suite your needs !
+              that provides you with an easy way to install Arch and a well optimized, beautifully Customized Plasma Desktop, that you can later shape to suite your needs!
               <br />
               <br />
               This Distro is not for "Everyone".<br />Basic Linux Knowledge is a Must-Have.
